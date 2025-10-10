88da932b63b6c8b294f0b2e4ee5dd20fd8534dc7
Chaitanya Pawar <144248006+ChaitanyaPawar1112@users.noreply.github.com>
2025-10-10 08:42:26 +0530

    Update Counter.jsx

--- Counter.jsx
+++ Counter.jsx
@@ -1,1 +1,34 @@
+//code for counter increament,decrement and recet
+import React ,{useState} from "react"
+function Counter()
+{
+let[value,setValue]=useState(0);
 
+const incrementByOne=()=>
+{
+    setValue(value+1);
+
+};
+const decrementByOne=()=>
+{
+    setValue(value-1);
+
+};
+
+const recet=()=>
+{
+    setValue(value=0);
+}
+    return(
+        <div>
+            <h2>count:{value}</h2>
+            <button onClick={incrementByOne}>+</button>
+             <button onClick={decrementByOne}>-</button>
+              <button onClick={recet}>recet</button>
+
+        </div>
+
+
+    );
+}
+export default Counter;
